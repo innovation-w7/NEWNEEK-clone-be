ff144e99d817b42530c4502c256606976201fe74
serapple <serinamgil@gmail.com>
2022-09-12 12:10:26 +0900

    News 전체조회, 개별 조회 구현
    News 전체조회, 개별 조회

--- src/main/java/com/innovation/newneekclone/controller/NewsController.java
+++ src/main/java/com/innovation/newneekclone/controller/NewsController.java
@@ -1,14 +1,30 @@
 package com.innovation.newneekclone.controller;
 
+import com.innovation.newneekclone.dto.ResponseDto;
+import com.innovation.newneekclone.entity.News;
 import com.innovation.newneekclone.service.NewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class NewsController {
 
     private final NewsService newsService;
+
+    @GetMapping("/api/news")
+    public ResponseDto<?> getAllNews() {
+        return newsService.getAllNews();
+    }
+
+    @GetMapping("/api/news/{newsid}")
+    public ResponseDto<?> getNews(@PathVariable Long newsid){
+        return newsService.getNews(newsid);
+    }
 
 
 }
--- src/main/java/com/innovation/newneekclone/service/NewsService.java
+++ src/main/java/com/innovation/newneekclone/service/NewsService.java
@@ -1,12 +1,40 @@
 package com.innovation.newneekclone.service;
 
+import com.innovation.newneekclone.dto.ResponseDto;
+import com.innovation.newneekclone.entity.News;
 import com.innovation.newneekclone.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class NewsService {
 
     private final NewsRepository newsRepository;
+
+    public ResponseDto<?> getAllNews() {
+        return ResponseDto.success(newsRepository.findAllByOrderByDate());
+    }
+
+    @Transactional
+    public ResponseDto<?> getNews(Long newsid) {
+//        News news = isPresentNews(newsid);
+//        if(news == null){
+//            return ResponseDto.fail("NOT_FOUNDED", "존재하지 않는 게시글");
+//        }
+        News news = newsRepository.findById(newsid).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 게시글입니다.")
+        );
+        return ResponseDto.success(news);
+    }
+
+//    @Transactional(readOnly = true)
+//    public News isPresentNews(Long newsid){
+//        Optional<News> optionalNews = newsRepository.findById(newsid);
+//        return optionalNews.orElse(null);
+//    }
 }
